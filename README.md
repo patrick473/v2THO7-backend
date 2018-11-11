06f953f4b7e120926ba5092fcbbe9e0405420f3e
patrick kottman <patkottman@gmail.com>
2018-11-11 18:52:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# v2THO7-backend
---
+++ src/main/java/App/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package App.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController{
+
+    @RequestMapping("/")
+    public String index(){
+        return "test test this is an endpoint";
+    }
+
+}
